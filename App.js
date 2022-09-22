1be9f9dd43962deefe72a45d6e9d63cc593bf26d
Daniel Skysľak <danilesky17@gmail.com>
2022-09-22 14:25:18 +0200

    Searchbar and body of the app

--- App.js
+++ App.js
@@ -5,11 +5,14 @@ import Search from "./src/components/Search";
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
         <Search />
-        <StatusBar style="auto" />
       </View>
+      <View style={styles.body}>
+        <Text>This is body of the App</Text>
+      </View>
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
@@ -17,8 +20,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
     backgroundColor: "#fff",
     alignItems: "center",
+  },
+  body: {
+    flex: 1,
     justifyContent: "center",
+    alignSelf: "stretch",
+    alignItems: "center",
+  },
+  search: {
+    alignSelf: "stretch",
+    padding: 10,
   },
 });
